818e2dd98f25f98997990acc511e492c6037cec1
elwolf6 <elwolf6@protonmail.com>
2022-07-18 21:22:51 -0400

    Artillery Fix

---
+++ common/defines/mod_defines.lua
@@ -0,0 +1,9 @@
+
+
+--Military & Combat defines
+NDefines.NMilitary.BACK_LINE_MORALE_DAMAGE_TAKEN_MODIFIER = 0.0	-- artillery takes 0% backrow morale dmg 
+NDefines.NMilitary.ARMY_DRILL_YEARLY_DECAY = 10000 --drill rework, drill increases naturally
+NDefines.NMilitary.MAX_DRILL_DECAY = 100 --drill rework, drill increases naturally
+NDefines.NMilitary.LOW_MORALE_THRESHOLD = 0.01 --changed from 0.5 so you can still move low morale units
+NDefines.NMilitary.COSSACKS_SHOCK_DAMAGE_BONUS = 0.0 --added elsewhere
+NDefines.NMilitary.STRELTSY_FIRE_DAMAGE_BONUS = 0.0 --added elsewhere
